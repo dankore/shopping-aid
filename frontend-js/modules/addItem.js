099dbe4985c83a43116abac4dba04ec0c0836418
Adamu M. Dankore <adamu.dankore@gmail.com>
2020-04-07 09:51:30 -0500

    items

--- frontend-js/modules/addItem.js
+++ frontend-js/modules/addItem.js
@@ -3,19 +3,35 @@ const axios = require("axios");
 export default class AddItem {
   constructor() {
     this.checkboxes = document.querySelectorAll("input[type=checkbox]");
+    this.displyaContainer = document.querySelector("#display-content");
+    this.submitBtn = document.querySelector("#submit-btn");
     this.events();
+    this.arr = [];
   }
   // EVENTS
   events() {
     Array.prototype.forEach.call(this.checkboxes, (checkbox) => {
       checkbox.addEventListener("click", (e) => this.handleClick(e));
     });
+    this.submitBtn.addEventListener("click", (e) => this.handleSubmit(e));
   }
   // METHODS
+  handleSubmit(e) {
+    axios.post("/add-items", { items: this.arr });
+  }
+
   handleClick(e) {
-    axios.post("/add-items", { item: e.target.value }).then((res) => {
-      console.log(res.data);
-    });
+    this.arr.push(e.target.value);
+
+    this.displyaContainer.insertAdjacentHTML(
+      "beforeend",
+      this.html(e.target.value)
+    );
+  }
+  html(item) {
+    return `
+    <p>${item}</p>
+    `;
   }
 
   // END CLASS
--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -2,8 +2,8 @@ const Item = require("../models/model");
 
 exports.home = (req, res) => {
   res.render("home");
-}
+};
 
 exports.addItem = (req, res) => {
-  console.log(req.body.item);
-}
+  console.log(req.body.items);
+};

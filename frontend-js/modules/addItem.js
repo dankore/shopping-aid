0c1c546b4c199e8dc1e47b04167e1ba9a90981bd
Adamu Muhammad Dankore <adamu.dankore@gmail.com>
2020-04-08 04:54:02 +0000

    toggle

--- frontend-js/modules/addItem.js
+++ frontend-js/modules/addItem.js
@@ -3,7 +3,8 @@ const axios = require("axios");
 export default class AddItem {
   constructor() {
     this.checkboxes = document.querySelectorAll("input[type=checkbox]");
-    this.displyaContainer = document.querySelector("#display-content");
+    this.checkboxesWrapper = document.querySelector("#checkboxes-wrapper");
+    this.categoryTitle = document.querySelectorAll("#category-title");
     this.submitBtn = document.querySelector("#submit-btn");
     this.events();
     this.arr = [];
@@ -13,9 +14,20 @@ export default class AddItem {
     Array.prototype.forEach.call(this.checkboxes, (checkbox) => {
       checkbox.addEventListener("click", (e) => this.handleClick(e));
     });
+    Array.prototype.forEach.call(this.categoryTitle, (title) => {
+      title.addEventListener("click", (e) => this.handleOpenClose(e));
+    });
     this.submitBtn.addEventListener("click", (e) => this.handleSubmit(e));
   }
   // METHODS
+  handleOpenClose(e){
+    if(this.checkboxesWrapper.style.display == "none"){
+      this.checkboxesWrapper.style.display = "block";
+    } else {
+      this.checkboxesWrapper.style.display = "none";
+    }
+  }
+  
   handleSubmit(e) {
     axios.post("/add-items", { items: this.arr });
   }

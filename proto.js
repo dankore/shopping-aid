5e96bd8208162d1a0c2f9af9efff1890ad4233c8
Adamu M. Dankore <adamu.dankore@gmail.com>
2020-04-09 22:24:40 -0500

    reverse algo n refac

--- proto.js
+++ proto.js
@@ -12,25 +12,35 @@ let arr = [
   { item: "b", categories: ["Fruits", "Vegetables"] },
 ];
 
-function sort(array){
-let fruits = [],
-  veg = [],
-  cond = [],
-  frozen = []
-
-for (let i = 0; i < array.length; i++) {
-  const element = array[i];
-  element.categories.indexOf("Fruits") != -1 && fruits.push(element.item);
-  element.categories.indexOf("Vegetables") != -1 && veg.push(element.item);
-  element.categories.indexOf("Condiments") != -1 && cond.push(element.item);
-  element.categories.indexOf("Frozen") != -1 && frozen.push(element.item);
+function reverse(array) {
+  for (let i = 0; i < array.length / 2; i++) {
+    let temp = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = temp;
+  }
+  return array;
 }
+console.log(reverse([1, 2, 7, 3]));
 
-// console.log(fruits);
-// console.log(veg);
-// console.log(cond);
-// console.log(frozen);
-return [fruits, veg, cond, frozen]
-}
+// function sort(array){
+// let fruits = [],
+//   veg = [],
+//   cond = [],
+//   frozen = []
 
-console.log(sort(arr));
+// for (let i = 0; i < array.length; i++) {
+//   const element = array[i];
+//   element.categories.indexOf("Fruits") != -1 && fruits.push(element.item);
+//   element.categories.indexOf("Vegetables") != -1 && veg.push(element.item);
+//   element.categories.indexOf("Condiments") != -1 && cond.push(element.item);
+//   element.categories.indexOf("Frozen") != -1 && frozen.push(element.item);
+// }
+
+// // console.log(fruits);
+// // console.log(veg);
+// // console.log(cond);
+// // console.log(frozen);
+// return [fruits, veg, cond, frozen]
+// }
+
+// console.log(sort(arr));
--- server/helpers/sortItems.js
+++ server/helpers/sortItems.js
@@ -7,11 +7,22 @@ module.exports = {
 
     for (let i = 0; i < array.length; i++) {
       array[i].categories.indexOf("Fruits") != -1 && fruits.push(array[i].item);
-      array[i].categories.indexOf("Vegetables") != -1 && veg.push(array[i].item);
-      array[i].categories.indexOf("Condiments") != -1 && cond.push(array[i].item);
+      array[i].categories.indexOf("Vegetables") != -1 &&
+        veg.push(array[i].item);
+      array[i].categories.indexOf("Condiments") != -1 &&
+        cond.push(array[i].item);
       array[i].categories.indexOf("Frozen") != -1 && frozen.push(array[i].item);
     }
 
-    return [fruits, veg, cond, frozen];
+    return [reverse(fruits), reverse(veg), reverse(cond), reverse(frozen)];
   },
 };
+
+function reverse(array) {
+  for (let i = 0; i < array.length / 2; i++) {
+    let temp = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = temp;
+  }
+  return array;
+}

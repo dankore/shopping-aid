4b091c5afb34a33311023b77b294f34517bb92a1
Adamu M. Dankore <adamu.dankore@gmail.com>
2020-04-13 07:59:22 -0500

    css

--- frontend-js/modules/selectItem.js
+++ frontend-js/modules/selectItem.js
@@ -5,7 +5,7 @@ export default class SelectItem {
     this.mainWrapper = document.querySelector("#main-wrapper");
     this.viewerContainer = document.querySelector("#viewer");
     this.titleBeforeSave = document.querySelector("#title-before-save");
-    this.listSectionImmediateChildElem = document.querySelector("#list-section-sub-element");
+    this.listSection = document.querySelector("#list-section");
     this.events();
     this.arr = [];
   }
@@ -55,10 +55,11 @@ export default class SelectItem {
         items: this.arr,
       })
       .then((res) => {
-        this.listSectionImmediateChildElem.insertAdjacentHTML(
-          "afterend",
-          this.html(res.data)
-        );
+        this.listSection.insertAdjacentHTML("afterbegin", this.html(res.data));
+        this.titleBeforeSave.value = "";
+        this.viewerContainer.innerHTML = "";
+        this.titleBeforeSave.focus();
+        this.arr = [];
       })
       .catch((err) => {
         alert(err);
@@ -67,26 +68,27 @@ export default class SelectItem {
   html(data) {
     return (
       `<div class="mb-5 rounded">
-            <div class="flex justify-between bg-gray-800 rounded-tr rounded-tl text-white cursor-pointer uppercase p-2">
+            <div class="flex justify-between bg-gray-800 rounded-tr rounded-tl text-white cursor-pointer uppercase">
               <h2
                 id="list-title"
+                class="w-full p-2"
               >
                 ${data[0].title}
               </h2>
-              <button id="delete-list" data-id="${data[0]._id}" class="px-2">X</button>
+              <button id="delete-list" data-id="${data[0]._id}" class="px-4 rounded-full hover:bg-gray-500">X</button>
             </div>
             <ul id="lists-wrapper" style="display: none;">` +
-              data[0].items
-                .map((item) => {
-                  return `<div
-                          class="flex justify-between border border-gray-200 bg-white px-2 py-1"
-                        >
-                          <li>${item}</li>
-                          <button class="text-red-600">Done</button>
-                        </div>`;
-                })
-                .join("") +
-              `
+      data[0].items
+        .map((item) => {
+          return `<div
+              class="flex justify-between border border-gray-200 bg-white px-2 py-1"
+            >
+              <li>${item}</li>
+              <button class="text-red-600">Done</button>
+            </div>`;
+        })
+        .join("") +
+      `
             </ul>
         </div>
     `
@@ -102,7 +104,7 @@ export default class SelectItem {
     // RENDER THE ARRAY
     let newArr = "";
     for (let i = 0; i < this.arr.length; i++) {
-      newArr += `<li>${this.arr[i]}</li>`;
+      newArr += `<span>${this.arr[i]}, </span>`;
     }
 
     this.viewerContainer.innerHTML = newArr;

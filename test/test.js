4e2ac86ddb75d000a15b95d1829c9d4f6bce21a7
Adamu M. Dankore <adamu.dankore@gmail.com>
2020-04-13 08:30:21 -0500

    test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 const assert = require("chai").assert;
-const App = require("../app");
+const uniqId = require("../server/helpers/uniqId");
 
-describe('model', function(){
-    it('model hello() should return hello', function(){
-        assert.equal(App(), "hello")
-    })
+describe('uniqId', function(){
+    it("uniqId() should return new Date().getTime()", function () {
+      assert.equal(uniqId(), new Date().getTime());
+    });
 })

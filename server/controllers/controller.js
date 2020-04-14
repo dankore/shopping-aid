0ee93d9bfed0745c84be85e985df1fd3b5561619
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-04-14 09:11:03 +0000

    refac module export

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,18 +1,18 @@
 const Item = require("../models/model");
-const { sortItems } = require("../helpers/sortItems");
-const { reverseIt } = require("../helpers/sortItems");
+
+const helper = require("../helpers/sortItems")
 const uniqId = require("../helpers/uniqId");
 
 exports.home = async (req, res) => {
   try {
     const items = await Item.getAll();
-    const sorted = sortItems(items);
+    const sorted = helper.sortItems(items);
     const lists = await Item.fetchSelectedItems();
 
     res.render("home", {
       fruits: sorted[0],
       veg: sorted[1],
-      lists: reverseIt(lists),
+      lists: helper.reverse(lists),
     });
   } catch (error) {
     console.log(error);

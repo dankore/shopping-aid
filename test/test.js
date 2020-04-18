344752948f35e1f4b5d4ecea2e8bcf9abbaba692
Adamu M. Dankore <adamu.dankore@gmail.com>
2020-04-18 03:44:15 -0500

    tests

--- test/test.js
+++ test/test.js
@@ -2,7 +2,6 @@ const assert = require("chai").assert;
 const uniqId = require("../server/helpers/uniqId");
 const helper = require("../server/helpers/sortItems");
 
-
 describe("uniqId", function () {
   it("Should return a string", function () {
     const result = uniqId();
@@ -16,10 +15,22 @@ describe("reverse", function () {
     assert.equal(result, 3);
   });
 });
-// TO COMPLETE
+
 describe("sortString", function () {
-  it("Should sort all elements in ascending order and return the first element", function () {
-    const result = helper.sortStrings(["b", "c", "a", "h"])[0];
+  it("Should sort all elements in ascending/alphabetical order and return the first element", function () {
+    const result = helper.sortStrings([
+      { id: "id", title: "Title of shopping list", items: ["c", "b", "a"] },
+    ])[0].items[0];
     assert.equal(result, "a");
+  });
+});
+describe("sortCategory", function () {
+  it("Should sort category in ascending/alphabetical order and return last element", function () {
+    const result = helper.sortCategory([
+      ["id2", "Broccoli", "Vegetables"],
+      ["id1", "Apple", "Fruits"],
+    ])[1][1];
+    console.log(result);
+    assert.equal(result, "Broccoli");
   });
 });

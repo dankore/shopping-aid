2033900958a0c2d72e022e0320478da79dc0b1df
Adamu M. Dankore <adamu.dankore@gmail.com>
2020-04-23 05:47:41 -0500

    update file(s)

--- frontend-js/modules/protectWithPassword-FE.js
+++ frontend-js/modules/protectWithPassword-FE.js
@@ -2,97 +2,73 @@ const axios = require("axios");
 
 export default class ProtectWithPassword {
   constructor() {
-    this.BtnModalProtectWithPassword = document.querySelector(
-      "#open-protect-with-password-modal"
-    );
-    this.modalProtectWithPassword = document.querySelector(
-      "#modal-protect-with-password"
-    );
+    this.listSectionWrapper = document.querySelector("#list-section");
     this.modalOverlay = document.querySelector(".modal-overlay");
-    this.cancelModalBtn = document.querySelector(
-      "#cancel-btn-protect-with-password"
-    );
-    this.deleteEntireShoppingListBtnPasswordProtected = document.querySelector(
-      "#delete-list-password-protected"
-    );
-    this.modalEnterPasswordShoppingList = document.querySelector(
-      "#modal-protect-with-password-enter-password"
-    );
-    this.cancelBtnEnterPasswordShoppingList = document.querySelector(
-      "#cancel-btn-protect-with-password-enter-password"
-    );
-    this.submitBtnEnterPasswordShoppingList = document.querySelector(
-      "#submit-btn-protect-with-password-enter-password"
-    );
-    this.inputEnterPasswordShoppingList = document.querySelector(
-      "#input-protect-with-password-enter-password"
-    );
+
     this.events();
   }
   // EVENTS
   events() {
-    this.BtnModalProtectWithPassword.addEventListener("click", () =>
-      this.handleOpenModalProtectWithPassword()
-    );
-    this.modalOverlay.addEventListener("click", () => this.handleCloseModals());
-    this.cancelModalBtn.addEventListener("click", () =>
-      this.handleCloseModals()
-    );
-    this.deleteEntireShoppingListBtnPasswordProtected &&
-      this.deleteEntireShoppingListBtnPasswordProtected.addEventListener(
-        "click",
-        () => this.handleOpenModalEnterPassword()
-      );
-    this.cancelBtnEnterPasswordShoppingList.addEventListener("click", () =>
-      this.handleCloseModals()
-    );
-    this.submitBtnEnterPasswordShoppingList.addEventListener("click", (e) =>
-      this.handleSubmitPasswordProtectedShoppingList(e)
-    );
+    this.listSectionWrapper.addEventListener("click", (e) => {
+      e.target && e.target.id == "overlay-modal" && this.handleCloseModals(e);
+      e.target &&
+        e.target.id == "delete-list-password-protected" &&
+        this.handleOpenModalEnterPassword(e);
+      e.target &&
+        e.target.id == "submit-btn-protect-with-password-enter-password" &&
+        this.handleSubmitPasswordProtectedShoppingList(e);
+      e.target &&
+        e.target.id == "modal-protect-with-password-enter-password" &&
+        this.handleSubmitPasswordProtectedShoppingList(e);
+      e.target &&
+        e.target.id == "cancel-btn-protect-with-password-enter-password" &&
+        this.handleCloseModals(e);
+      e.target &&
+        e.target.style.display == "block" &&
+        e.target.id == "submit-btn-protect-with-password-enter-password" &&
+        this.handleSubmitPasswordProtectedShoppingList(e);
+    });
   }
   // METHODS
   handleSubmitPasswordProtectedShoppingList(e) {
-    if (this.inputEnterPasswordShoppingList.value == "") return;
+    if (e.target.parentElement.children[0].value == "") return;
     axios
       .post("/delete-shopping-list", {
-        password: this.inputEnterPasswordShoppingList.value,
-        id: this.deleteEntireShoppingListBtnPasswordProtected.getAttribute(
+        password: e.target.parentElement.children[0].value,
+        id: e.target.parentElement.parentElement.children[0].children[1].getAttribute(
           "data-id"
         ),
       })
       .then((res) => {
-        console.log(res.data);
-        console.log(e.target);
-        // e.target.parentElement.parentElement.remove();
+        e.target.parentElement.parentElement.remove();
       })
       .catch((err) => {
         alert(err);
       });
   }
 
-  handleOpenModalEnterPassword() {
-    if (this.modalEnterPasswordShoppingList.style.display == "none") {
-      this.modalEnterPasswordShoppingList.style.display = "block";
+  handleOpenModalEnterPassword(e) {
+    if (
+      e.target.parentElement.parentElement.children[2].style.display == "none"
+    ) {
+      e.target.parentElement.parentElement.children[2].style.display = "block";
       this.modalOverlay.classList.add("active");
-      this.modalEnterPasswordShoppingList.classList.add("active");
+      e.target.parentElement.parentElement.children[2].classList.add("active");
     }
   }
 
-  handleCloseModals() {
-    this.modalProtectWithPassword &&
-      (this.modalProtectWithPassword.style.display = "none");
+  handleCloseModals(e) {
+    e.target.parentElement.style.display = "none";
     this.modalOverlay.classList.remove("active");
-    this.modalEnterPasswordShoppingList &&
-      (this.modalEnterPasswordShoppingList.style.display = "none");
   }
 
-  handleOpenModalProtectWithPassword() {
-    if (this.modalProtectWithPassword.style.display == "none") {
-      this.modalProtectWithPassword.style.display = "block";
-      this.modalOverlay.classList.add("active");
-      this.modalProtectWithPassword.classList.add("active");
-    }
-  }
+  // handleOpenModalProtectWithPassword() {
+  //   if (this.modalProtectWithPassword.style.display == "none") {
+  //     this.modalProtectWithPassword.style.display = "block";
+  //     this.modalOverlay.classList.add("active");
+  //     this.modalProtectWithPassword.classList.add("active");
+  //   }
+  // }
 
   // END CLASS
 }
